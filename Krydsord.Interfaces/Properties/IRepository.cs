61fb496febc3c939aa204f240dc527a6467bcb29
theskov <philipskov@gmail.com>
2013-07-09 22:04:48 +0200

    Resterende filer

---
+++ Krydsord.Interfaces/Properties/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krydsord.Interfaces
+{
+    public interface IRepository<T>
+    {
+        T Get(string sti);
+    }
+}
